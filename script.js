89ad81a234008480622cf655420d3fbacbcbce6e
rvolkov8 <120594195+rvolkov8@users.noreply.github.com>
2023-01-24 21:49:39 +0200

    Create a 16x16 grid of square divs in container

---
+++ script.js
@@ -0,0 +1,12 @@
+const container = document.querySelector("#container");
+
+container.style.height = "960px";
+container.style.width = "960px";
+container.style.display = "grid";
+
+for (let i = 0; i < 256; i++) {
+    const div = document.createElement("div");
+    container.appendChild(div);
+}
+container.style.gridTemplateColumns = `repeat(16, 1fr)`;
+container.style.gridTemplateRows = `repeat(16, 1fr)`;
