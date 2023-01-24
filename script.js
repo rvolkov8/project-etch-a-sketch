f23df10c27091ce08d985aceb6d9c975b96df76d
rvolkov8 <120594195+rvolkov8@users.noreply.github.com>
2023-01-24 22:00:53 +0200

    Add a Create canvas button
    the button listens for a click to:
    - reset the previous canvas
    - create a popup asking the user for the number of squares per side for the new grid (limited between 1 and 100)
    - generate a new grid with the same total space as before

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
 const container = document.querySelector("#container");
+const createCanvasBtn = document.querySelector("#create-canvas");
 
 container.style.height = "960px";
 container.style.width = "960px";
 container.style.display = "grid";
 
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement("div");
-    div.addEventListener("mouseover", function() {
-        div.style.backgroundColor = `black`
-    });
-    container.appendChild(div);
+createCanvasBtn.addEventListener("click", () => {
+    resetCanvas();
+    let cellNum = prompt("Enter a number of squares per side", "");
+    while (isNaN(cellNum) === true || cellNum > 100 || cellNum < 1) {
+        alert("Please enter a valid number between 1 and 100")
+        cellNum = prompt("Enter a number of squares per side", "");
+    }
+    createGrid(cellNum);
+});
+
+function createGrid(cellNum) {
+    for (let i = 0; i < cellNum * cellNum; i++) {
+        const div = document.createElement("div");
+        div.addEventListener("mouseover", function() {
+            div.style.backgroundColor = `black`
+        });
+        container.appendChild(div);
+    }
+    container.style.gridTemplateColumns = `repeat(${cellNum}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${cellNum}, 1fr)`
 }
-container.style.gridTemplateColumns = `repeat(16, 1fr)`;
-container.style.gridTemplateRows = `repeat(16, 1fr)`;
+
+function resetCanvas() {
+    container.innerHTML = '';
+}
+
+
+
+
+
+
+
+
+
+
+
